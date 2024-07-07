f43cf5ec4fccad485d6b3d3c9eb16ac613e8c338
Abdelmounim Baouia <abdelmounimbaouia@gmail.com>
2024-07-07 08:55:37 +0100

    connecting to db

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 
 	"github.com/gorilla/mux"
+	"github.com/joho/godotenv"
 )
 
 func main() {
@@ -13,5 +19,37 @@ func main() {
 		vars := mux.Vars(r)
 		fmt.Fprintf(w, "you requested book %s, page %s", vars["title"], vars["page"])
 	})
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("couldn't load env vars")
+	}
+
+	connStr := os.Getenv("CONN")
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal("Couldn't connect to the db", err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		log.Fatal("db connection not working", err)
+	}
+
+	query := `
+    CREATE TABLE IF NOT EXISTS users (
+      id serial primary key,
+      username text not null,
+      password text not null,
+      created_at date
+    );
+  `
+
+	_, err = db.Exec(query)
+	if err != nil {
+		log.Fatal("error creating users table: ", err)
+	}
+
 	http.ListenAndServe(":3000", router)
 }
